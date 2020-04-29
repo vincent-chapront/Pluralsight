2fa9e3cf3a89cb171d147a314d06e8223ecb1465
Vincent Chapront <vincent.chapront@gmail.com>
2020-04-29 20:49:30 +0200

    3-5 Demo: Accessing Data from the REST API

--- Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Startup.cs
+++ Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShopHRM.Server.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -26,7 +27,12 @@ namespace BethanysPieShopHRM.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(
+                client=>
+                {
+                    client.BaseAddress = new Uri("https://localhost:44340");
+                });
+            services.AddServerSideBlazor().AddCircuitOptions(options=>options.DetailedErrors=true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
