dfbba086a355f069eff97584a1534c103fb9d017
Vincent Chapront <vincent.chapront@gmail.com>
2020-04-30 07:48:15 +0200

    3-10 Demo: Adding the Add Employee Form

---
+++ Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Pages/EmployeeEditBase.cs
@@ -0,0 +1,26 @@
+﻿using BethanysPieShopHRM.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Pages
+{
+    public class EmployeeEditBase:ComponentBase
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+    }
+}
