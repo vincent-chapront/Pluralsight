e07329fe531d36401649380066cb0cb23497ce25
Vincent Chapront <vincent.chapront@gmail.com>
2020-05-03 10:30:24 +0200

    3-11 Demo: Savig the data

--- Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Pages/EmployeeEditBase.cs
+++ Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Pages/EmployeeEditBase.cs
@@ -16,7 +16,8 @@ namespace BethanysPieShopHRM.Server.Pages
         public ICountryDataService CountryDataService { get; set; }
         [Inject]
         public IJobCategoryDataService JobCategoryDataService{ get; set; }
-
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         [Parameter]
         public string EmployeeId { get; set; }
 
@@ -25,13 +26,73 @@ namespace BethanysPieShopHRM.Server.Pages
         public List<JobCategory> JobCategories{ get; set; } = new List<JobCategory>();
         protected string CountryId = string.Empty;
         protected string JobCategoryId = string.Empty;
+
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
+
         protected async override Task OnInitializedAsync()
         {
-            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            Saved = false;
+            //Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
             Countries = (await CountryDataService.GetAllCountries()).ToList();
             JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+
+            int.TryParse(EmployeeId, out var employeeId);
+            if (employeeId == 0)
+            {
+                Employee = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now };
+            }
+            else
+            {
+                Employee = await EmployeeDataService.GetEmployeeDetails(employeeId);
+            }
             CountryId = Employee.CountryId.ToString();
             JobCategoryId = Employee.JobCategoryId.ToString();
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            Saved = false;
+            Employee.CountryId = int.Parse(CountryId);
+            Employee.JobCategoryId = int.Parse(JobCategoryId);
+
+            if (Employee.EmployeeId == 0) //new
+            {
+                var addedEmployee = await EmployeeDataService.AddEmployee(Employee);
+                if (addedEmployee != null)
+                {
+                    StatusClass = "alert-success";
+                    Message = "New employee added successfully.";
+                    Saved = true;
+                }
+                else
+                {
+                    StatusClass = "alert-danger";
+                    Message = "Something went wrong adding the new employee. Please try again.";
+                    Saved = false;
+                }
+            }
+            else
+            {
+                await EmployeeDataService.UpdateEmployee(Employee);
+                StatusClass = "alert-success";
+                Message = "Employee updated successfully.";
+                Saved = true;
+            }
+        }
+        protected async Task DeleteEmployee()
+        {
+            await EmployeeDataService.DeleteEmployee(Employee.EmployeeId);
+
+            StatusClass = "alert-success";
+            Message = "Deleted succesfully";
+
+            Saved = true;
+        }
+        protected void NavigateToOverview()
+        {
+            NavigationManager.NavigateTo("/employeeoverview");
         }
     }
 }
--- Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Services/EmployeeDataService.cs
+++ Building Web Applications with Blazor/Blazor_ Getting Started/BethanysPieShopHRM/BethanysPieShopHRM.Server/Services/EmployeeDataService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -17,16 +18,6 @@ namespace BethanysPieShopHRM.Server.Services
             _httpClient = httpClient;
         }
 
-        public Task<Employee> AddEmployee(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteEmployee(int employeeId)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<IEnumerable<Employee>> GetAllEmployees()
         {
             return await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
@@ -38,10 +29,32 @@ namespace BethanysPieShopHRM.Server.Services
             return await JsonSerializer.DeserializeAsync<Employee>
                (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
-
-        public Task UpdateEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/employee", employeeJson);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+            }
+
+            return null;
+        }
+
+        public async Task UpdateEmployee(Employee employee)
+        {
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            await _httpClient.PutAsync("api/employee", employeeJson);
+        }
+
+        public async Task DeleteEmployee(int employeeId)
+        {
+            await _httpClient.DeleteAsync($"api/employee/{employeeId}");
         }
     }
 }
